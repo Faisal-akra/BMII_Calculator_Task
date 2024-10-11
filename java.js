94dc06033312cb131592497e1900c4c203853443
Faisal-Akram <zjan45097@gmail.com>
2024-10-11 21:19:50 +0500

    Condition agar valid weight & hight nahi to alertt main  msg dedo

--- java.js
+++ java.js
@@ -6,6 +6,13 @@ function calculateBMI() {
 var weight = document.getElementById('weight').value;
 var height = document.getElementById('height').value;
 
+//Check condition agar user nay number enter nahi kya to alert kardo:
+
+if(isNaN(weight) || isNaN(height) || height<=0 || weight<=0) {
+    alert("plese enter valid height and weight")
+    return;
+}
+
 
 
 }
